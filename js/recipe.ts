001a6502a2e3a180ce55cf68e3d7ec93ed95a759
Stebenwolf <benjamin@the-org.eu>
2022-03-18 15:07:34 +0100

    Add illustration for pictures

--- js/recipe.ts
+++ js/recipe.ts
@@ -78,9 +78,9 @@ class Recipe {
         divCard.setAttribute("style","");
 
         const divCardImg = document.createElement("img");
-        divCardImg.src = "";
+        divCardImg.src = "../assets/img/"+this.id+".jpg";
         divCardImg.className = "card-img-top";
-        divCardImg.alt = "";
+        divCardImg.alt = this.name;
 
         const divCardBody = document.createElement("div");
         divCardBody.className = "card-body";
